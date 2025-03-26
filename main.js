52e62fcb747835d6dffccc684e175f84b11a6839
jihyeon <wlgusl147@naver.com>
2025-03-26 16:54:08 +0900

    변경

---
+++ main.js
@@ -0,0 +1,10 @@
+const shortcut = document.querySelector(".shortcut");
+
+shortcut.addEventListener("click mouseenter", showLayer);
+
+function showLayer(){
+   const layer1 = document.querySelector("#layer1");
+   console.log()
+   
+
+}
